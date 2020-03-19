eb3b37c66a8b712605f93f6f2f382173bcfc9092
vladatiglau <57870904+vladatiglau@users.noreply.github.com>
2020-03-19 19:54:18 +0200

    Update TestGit.java

--- TestGit.java
+++ TestGit.java
@@ -1,12 +1,22 @@
 package tema;
 
+import java.util.ArrayList;
+import java.util.List;
+import dv.Client;
+
+
 
 public class TestGit  {
 	
     
-    public static void main(String[] args) {  
-       
-            System.out.println("Hello Git! Denumirea proiectului de licență este Sistem software pentru managementul relațiilor cu clienții");
+    public static void main(String[] args) {
+    	
+    	Client c = new Client (1203, "Petrescu Ion");
+    	List<Client> lista = new ArrayList<Client>();
+    	
+		lista.add(c);
+
+            System.out.println("Hello Git! Denumirea proiectului de licenta este Sistem software pentru managementul relatiilor cu clientii" + lista);
         }
        
 }
