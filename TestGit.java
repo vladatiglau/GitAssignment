85f5051bfba37fe774602f0566a6c85b5607246c
vladatiglau <57870904+vladatiglau@users.noreply.github.com>
2020-03-19 19:47:38 +0200

    Create TestGit.java

---
+++ TestGit.java
@@ -0,0 +1,12 @@
+package tema;
+
+
+public class TestGit  {
+	
+    
+    public static void main(String[] args) {  
+       
+            System.out.println("Hello Git! Denumirea proiectului de licență este Sistem software pentru managementul relațiilor cu clienții");
+        }
+       
+}
